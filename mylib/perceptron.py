c1babff7398364108ef51baf0d746188241257c0
andreas <podovinov2003andrey@gmail.com>
2023-04-19 12:56:12 +0600

    perceptron work so-so

--- mylib/perceptron.py
+++ mylib/perceptron.py
@@ -1,35 +1,36 @@
 import numpy as np
 import numdifftools as ndt
-from math import sqrt
 from typing import List
 
 
 class Layer:
     def __init__(self, output_dim: int, activation: callable, input_dim: int = None):
         self.output_dim = output_dim
-        self.activation = activation
+        self.activation = activation        # activation = f1(T) -- функция активации
         self.input_dim = input_dim
+        self.H0 = None                      # H0 @ W1 + b1 = T1
+        self.T = None                       # f1(T1) = H1
         self.W = None
-        self.X = None
-        self.X_past = None
+        self.b = None
 
     def forward(self, X: np.ndarray) -> np.ndarray:
-        # self.W = self.W / self.W  # !
-        self.X_past = X
-        self.X = self.activation(X @ self.W)
-        return self.X
+        self.H0 = X
+        self.T = self.H0 @ self.W + self.b
+        return self.activation(self.T)      # return H1
 
-    def backward(self, loss: callable, y_true: np.ndarray, bw: np.ndarray = None, alpha: float = 0.01) -> np.ndarray:
-        if bw is None:
-            bw = ndt.Gradient(loss)(self.X, y_true)#.reshape(-1, 1)
-        print(self.X_past.shape, self.W.shape)
-        act_diff = ndt.Derivative(self.activation)(self.X_past @ self.W)
-        adamar = act_diff * bw
-        grad = self.X_past.T @ adamar
-        self.W -= alpha * grad
-        print('!')
-        print(adamar.shape, self.W.shape)
-        return adamar @ self.W.T
+    def backward(self, dE_dh: np.ndarray, alpha: float = 0.01) -> np.ndarray:
+        dh_dt = ndt.Derivative(self.activation)(np.mean(self.T, axis=0))[np.newaxis, :]
+        dE_dt = dE_dh * dh_dt
+
+        dE_db = dE_dt
+        self.b -= alpha * dE_db
+
+        meanH0 = np.mean(self.H0, axis=0)[np.newaxis, :]
+        dE_dW = (meanH0.T @ dE_dt)
+        self.W -= alpha * dE_dW
+
+        dE_dH0 = dE_dt @ self.W.T
+        return dE_dH0
 
 
 class Perceptron:
@@ -42,66 +43,85 @@ class Perceptron:
     def compile(self, loss: callable):
         self.loss = loss
         input_dim = self.layers[0].input_dim
+
         for layer in self.layers:
             output_dim = layer.output_dim
-            layer.W = np.vstack((np.random.randn(input_dim, output_dim), np.ones(output_dim)))
-            input_dim = layer.output_dim
+            layer.W = np.random.randn(input_dim, output_dim, )
+            layer.b = np.ones(output_dim, dtype=float)[np.newaxis, :]
+            input_dim = output_dim
+
         return self
 
-    def fit(self, X: np.ndarray, y: np.ndarray, epochs: int = 1):
+    def fit(self, X: np.ndarray, y: np.ndarray, epochs: int = 1, alpha=0.01):
         for _ in range(epochs):
             print(f'epoch {_ + 1}/{epochs}')
             y_pred = self.forward(X, self.layers)
-            self.backward(y, y_pred, self.layers)
+            # ! для классификации что с y делать?
+            # TODO: тут преобразовать y
+            self.backward(y_true=y, y_pred=y_pred, layers=self.layers, alpha=alpha)
+
         return self
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         return self.forward(X, self.layers)
 
     def forward(self, X: np.ndarray, layers: List[Layer]) -> np.ndarray:
-        cur_X = X
+        pred = X
         for layer in layers:
-            cur_X = np.c_[cur_X, np.ones((cur_X.shape[0], 1))]
-            cur_X = layer.forward(cur_X)
-            # print(cur_X)  # !
+            pred = layer.forward(pred)
         print('===== Forward pass ended =====')
-        return cur_X
+        return pred
 
-    def backward(self, y_true: np.ndarray, y_pred: np.ndarray, layers: List[Layer]):
-        print('loss:', self.loss(y_true, y_pred))
-        bw = None
+    def backward(self, y_true: np.ndarray, y_pred: np.ndarray, layers: List[Layer], alpha=0.01):
+        # print('loss:', self.loss(y_true, y_pred))  # ! ? ? ? ? ? ?
+        y_true = y_true.reshape(-1, 1)
+        dE_dh = np.mean(ndt.Derivative(lambda pred: self.loss(y_true, pred))(y_pred), axis=0)
+        dE_dh = dE_dh[np.newaxis, :]
         for layer in layers[::-1]:
-            bw = layer.backward(self.loss, y_true=y_true, bw=bw)
+            dE_dh = layer.backward(dE_dh, alpha=alpha)
         print('===== Backward pass ended =====')
 
 
 X = np.array([
     [1, 2, 3],
-    [5, 6, 7],
+    [3, 2, 2],
+    [1, 0, 1],
+    [4, 0, 0],
 ])
 
 y = np.array([
-    2,
-    6
+    1, 1, 0, 1
 ])
 
-p = Perceptron([
-    Layer(5, lambda X: X, 3),
-    Layer(3, lambda X: X),
-    Layer(2, lambda X: X),
-    Layer(1, lambda X: X),
-]).compile(lambda x, y: np.sum((x - y)**2))
+# p = Perceptron([
+#     Layer(5, lambda X: np.maximum(0, X), 3),
+#     # Layer(3, lambda X: X),
+#     Layer(2, lambda X: np.maximum(0, X)),
+#     Layer(1, lambda X: np.maximum(0, X)),
+# ]).compile(lambda true, pred: (pred - true)**2)
 
 
-# act = lambda X: 2*X + X*X  # 2 + 2x
-# grad = ndt.Gradient(act)
-# print(ndt.Derivative(act)(X))
+# p.fit(X, y)
 
-# loss = lambda X, Y: np.sum((X - Y)**2)
-# print(np.array([[1, 1],[1, 1],[1, 1],[3, 3],[5, 5],[1, 1],]) * ndt.Gradient(loss)([1, 1, 1, 4, 4, 2], [1, 1, 1, 9, 9, 5]).reshape(-1, 1))
+# ls = lambda x, y: np.sum((x - y)**2)
+# y_true = np.array([[1], [1]])
+# y_pred = np.array([[4], [2]])
 
-p.fit(X, y)
-# print(ndt.Gradient(lambda x: x[0]*10 + x[0]*x[1])([3, 4]))
-# print(ndt.Derivative(lambda x, y: x*10 + x*y)(3, 4))
-
-print('пук')
+# x = np.array([[1, 0],
+#               [0, 3],
+#               [5, 1],
+#               [0, 1],])
+# y = np.array([
+#               [1, 0],
+#               [0, 1],
+#               [0, 1],
+#               [0, 1],
+# ])
+# E = lambda true, pred: (pred - true)**2
+# Q = lambda Y, X: np.sum(E(Y, X)) / Y.shape[0]
+# # print(
+# #     np.mean(ndt.Derivative(lambda pred: E(y, pred))(x), axis=0)
+# # )
+# # print(
+# #     np.mean(ndt.Derivative(lambda pred: E(y_true, pred))(y_pred), axis=0)
+# # )
